db36074690c81e5fd33255e0ac0dda22775ae281
ooishujing <18016012@myrp.edu.sg>
2019-08-02 12:13:24 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/demomywebbrowser/MainActivity.java
@@ -0,0 +1,37 @@
+package sg.edu.rp.c346.demomywebbrowser;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnLoadURL;
+    WebView wvMyPage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnLoadURL=findViewById(R.id.buttonLoad);
+        wvMyPage = findViewById(R.id.webViewMyPage);
+        wvMyPage.setWebViewClient(new WebViewClient());
+
+        btnLoadURL.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://www.grab.com/sg/";
+                wvMyPage.loadUrl(url);
+                WebSettings lol = wvMyPage.getSettings();
+                lol.setJavaScriptEnabled(true);
+                lol.setAllowFileAccess(false);
+                lol.setDisplayZoomControls(true);
+
+            }
+        });
+    }
+}
